acd391c6c1c3180c8efcd02a83b9181159833537
juguenkuzu <juguenkuzu@gmail.com>
2021-11-17 12:54:56 -0500

    first commit

---
+++ Action.c
@@ -0,0 +1,69 @@
+Action()
+{
+
+	web_set_sockets_option("SSL_VERSION", "AUTO");
+
+	web_add_auto_header("Sec-Fetch-Site", 
+		"none");
+
+	web_add_auto_header("Sec-Fetch-Mode", 
+		"navigate");
+
+	web_add_auto_header("Sec-Fetch-Dest", 
+		"document");
+
+	web_add_auto_header("Sec-Fetch-User", 
+		"?1");
+
+	web_add_auto_header("Upgrade-Insecure-Requests", 
+		"1");
+
+	web_add_auto_header("sec-ch-ua", 
+		"\"Google Chrome\";v=\"95\", \"Chromium\";v=\"95\", \";Not A Brand\";v=\"99\"");
+
+	web_add_auto_header("sec-ch-ua-mobile", 
+		"?0");
+
+	web_add_auto_header("sec-ch-ua-platform", 
+		"\"Windows\"");
+
+	web_url("blazedemo.com", 
+		"URL=https://blazedemo.com/", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=", 
+		"Snapshot=t1.inf", 
+		"Mode=HTML", 
+		EXTRARES, 
+		"Url=/favicon.ico", ENDITEM, 
+		LAST);
+
+	/* select departure and destination city */
+
+	web_add_auto_header("Origin", 
+		"https://blazedemo.com");
+
+	web_add_auto_header("Sec-Fetch-Site", 
+		"same-origin");
+
+	lr_think_time(46);
+
+		ITEMDATA, 
+	web_submit_form("reserve.php", 
+		"Snapshot=t2.inf", 
+		ITEMDATA, 
+		"Name=fromPort", "Value=Boston", ENDITEM, 
+		"Name=toPort", "Value=London", ENDITEM, 
+		LAST);
+
+	/* choose Compagny fly  */
+
+	lr_think_time(22);
+
+	web_submit_form("purchase.php", 
+		"Ordinal=2", 
+		"Snapshot=t3.inf", 
+		LAST);
+
+	return 0;
+}
